f69aede0328a9d0bc4269e2c427da5baf831ceaf
Morris <ernestres.a@hotmail.com>
2025-07-01 21:01:16 -0600

    feat: add language, optimize menu toggle script and translate to spanish
    added language selector with menu, removed unused section from page,
    optimized menu toggle script for reuse, translated entire page into
    spanish

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,12 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x")
-    navbar.classList.toggle("active")
-}
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 200;
+        let offset = sec.offsetTop - 300;
         let height = sec.offsetHeight;
         let id = sec.getAttribute("id");
 
@@ -24,4 +19,31 @@ window.onscroll = () => {
     });
     navbar.classList.remove("active");
     menuIcon.classList.remove("bx-x")
+}
+
+// Menu toggle
+document.querySelectorAll(".menu-wrapper").forEach(wrapper => {
+    const toggle = wrapper.querySelector(".menu-toggle");
+    const menu = wrapper.querySelector(".menu-options");
+
+    toggle.addEventListener('click', (e) => {
+        e.stopPropagation();
+        closeAllMenusExcept(wrapper);
+        menu.classList.toggle("active");
+    })
+});
+
+// Close menus when click outside
+document.addEventListener('click', () => {
+    document.querySelectorAll(".menu-options.active").forEach(menu => {
+        menu.classList.remove("active");
+    })
+});
+
+function closeAllMenusExcept(exception) {
+    document.querySelectorAll(".menu-wrapper").forEach(wrapper => {
+        if (wrapper !== exception) {
+            wrapper.querySelector(".menu-options").classList.remove('active')
+        }
+    })
 }
